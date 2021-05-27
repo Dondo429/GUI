f8859383fbc7238f94ae2e5a30399d58741b6293
Dondo429 <74946158+Dondo429@users.noreply.github.com>
2021-05-26 18:27:31 -0700

    Add files via upload

---
+++ clear.js
@@ -0,0 +1,22 @@
+var gl = null;
+var cone = null;
+
+function init() {
+    var canvas = document.getElementById( "webgl-canvas" );
+
+    gl = WebGLUtils.setupWebGL( canvas );
+    if ( !gl ) {
+        alert("Unable to setup WebGL");
+        return;
+    }
+
+    gl.clearColor( 1.0, 0.0, 1.0, 1.0 );
+	Cone(gl);
+    render();
+}
+
+function render() {
+	cone.render();
+}
+
+window.onload = init;
