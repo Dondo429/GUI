20027dbbe02267e723cc8d79b12ea5439f2137ef
Dondo429 <74946158+Dondo429@users.noreply.github.com>
2021-06-14 21:12:53 -0700

    finished assignment 5

---
+++ SolarSystem.js
@@ -0,0 +1,69 @@
+//changed values to render solar system correctly
+var SolarSystem = {
+  Sun : {
+    radius : 4.298,  
+    distance : 0,
+    year : 0,
+    color : [ 1.0, 1.0, 0.0, 1.0 ]
+  },
+  Mercury : {
+    radius : 0.383,
+    distance : 0.390,
+    year : 0.241,
+    color : [ 1.0, 0.0, 0.0, 1.0 ]
+  },
+  Venus : {
+    radius : 0.950,
+    distance : 0.720,
+    year : 0.616,
+    color : [ 1.0, 0.0, 1.0, 1.0 ]
+  },
+  Earth : {
+    radius : 1,
+    distance : 1,
+    year : 1,
+    color : [ 0.0, 0.0, 1.0, 1.0 ]
+  },
+  Moon : {
+    radius : 0.273,
+    distance : 0.00257,
+    year : 0.0748,
+    color : [ 0.5, 0.5, 0.5, 1.0 ]
+  },
+  Mars : {
+    radius : 0.532,
+    distance : 1.520,
+    year : 1.88,
+    color : [ 1.0, 0.0, 0.0, 1.0 ]
+  },
+  Jupiter : {
+    radius : 10.973,
+    distance : 5.20,
+    year : 11.871,
+    color : [ 1.0, 153/255, 0.0, 1.0 ]
+  },
+  Saturn : {
+    radius : 9.140,
+    distance : 9.50,
+    year : 29.477,
+    color : [ 1.0, 1, 0.0, 1.0 ]
+  },
+  Uranus : {
+    radius : 3.981,
+    distance : 19.20,
+    year : 84.074,
+    color : [ 0.0, 0.1, 1.0, 1.0 ]
+  },
+  Neptune : {
+    radius : 3.865,
+    distance : 30.10,
+    year : 163.904,
+    color : [ 0.0, 0.0, 1.0, 1.0 ]
+  },
+  Pluto : {
+    radius : 0.187,
+    distance : 39.48,
+    year : 247.9,
+    color : [ 150/255, 75/255, 0.0, 1.0 ]
+  }
+};
